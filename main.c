0682a96f7ca3ed5a4460b076c1151b6b02848342
rui0926 <43432504+rui0926@users.noreply.github.com>
2018-10-05 17:43:33 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,9 +3,33 @@
 int main(int argc, char *argv[]) 
 {
     char str[5];
+    array[100];
     
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+    while(fgets(str, sizeof(str), stdin))
+    {
+       inputnum = 0;
+        
+        void efface()
+        if (str == -1)
+        {
+          printf("%s\n", array[-insert-1]);
+            efface
+        }
+         else if(str == 0)
+         {
+             for(i = 0, )
+             {
+                 if(i == 0)
+                 printf("%s")
+             }
+             printf("%s", str);
+         }
+        else
+        {
+            array[inputnum++] = insert;
+        }
+        
+       
     }
 
     return 0;
