c47d4438910ca807251d0de00dc8982d816588a6
rui0926 <43432504+rui0926@users.noreply.github.com>
2018-10-05 19:52:09 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+ 
+void efface(int *array[-insert-1], int *array[insert])
+{
+    int temp;
+    temp = *array[-insert-1];
+    *array[-insert-1] = *array[insert];
+    
 int main(int argc, char *argv[]) 
+
 {
     char str[5];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+    array[100];
+     
+    while(fgets(str, sizeof(str), stdin))
+
+    {
+
+       inputnum = 0;
+       int input = atoi(str);
+
+        void efface(*)
+            
+        {
+            retturn;
+        }
+        
+        if (input == -1)
+        {
+          printf("%s\n", array[-insert-1]);
+            efface
+        }
+         
+        else if(input == 0)
+         {
+             for(i = 0, 0 <= array[insert], i++)
+             {
+                 if(i == 0)
+                 printf("%s\n",array[0]);
+                 
+                 else
+                 sprintf(insert, "%s",array[i]);
+                 
+                 printf("\n");
+             }
+          }
+             printf("%s", str);
+         }
+
+        else
+        {
+            array[inputnum++] = insert;
+        }
+
     }
 
+
+
     return 0;
+
 }
